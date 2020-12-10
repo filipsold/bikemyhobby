8328f39bd5de783d4d635e7e603d7531ec2a50ab
Filippos <filipsoldatos@gmail.com>
2020-12-10 11:43:59 +0200

    Images changes

--- css/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).docready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").onclick(function(){
-        $("#mycarousel").carousel('pause');
-    });
-    $("#carousel-play").onclick(function(){
-        $("#mycarousel").carousel('cycle');
-    });
-});
